4ed0f2202347da8a243f3ff9a348c6a65bb97e3e
BENONI ESCKINDER <besckinder@gmail.com>
2024-10-26 22:50:42 +0300

    Fixed button on New User page + New task page done

--- rtime_craft/src/pages/NewUser.jsx
+++ rtime_craft/src/pages/NewUser.jsx
@@ -20,11 +20,11 @@ export default function NewUser() {
         </div>
         <div className='flex justify-center absolute left-24 md:mt-0 w-[73%] ml-1 max-w-[700px]'>
           <Button variant='outline' 
-            className=' px-4 py-2 md:py-6 rounded-md shadow-lg font-monomaniac border border-gray-500 text-gray-500 hover:text-black hover:border-black md:text-3xl md:px-7 h-fit'>
+            className=' px-4 py-2 md:py-6 rounded-md shadow-lg font-madimi border border-gray-500 text-gray-500 hover:text-black hover:border-black md:text-3xl md:px-7 h-fit'>
             Create user
           </Button>
           <Button variant='default' 
-            className='ml-2 bg-yellow1 px-4 py-2 md:py-6 rounded-md shadow-lg font-monomaniac text-white md:text-3xl md:px-7 h-fit'>
+            className='ml-2 bg-yellow1 px-4 py-2 md:py-6 rounded-md shadow-lg font-madimi text-white md:text-3xl md:px-7 h-fit'>
             Sign up instead
           </Button>
       </div>
