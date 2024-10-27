a12c8b68cd07e2219c04f9086a8b7c6b5ed0fa2e
BENONI ESCKINDER <you@example.com>
2024-10-27 17:49:51 +0300

    Settings page done

--- rtime_craft/src/App.jsx
+++ rtime_craft/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react";
 import Sidebar from "./components/Sidebar"
 import Main from "./components/Main"
 import Split from "react-split"
@@ -12,20 +12,15 @@ import MonthlyReport from "./pages/MonthlyReport"
 import TotalProductiveTime from "./pages/TotalProductiveTime"
 import TotalWastedTime from "./pages/TotalWastedTime"
 import TotalTimeOnTask from "./pages/TotalTimeOnTask"
-import Profile from "./pages/Profile"
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings"
 // import "./style.css"
 
 
 export default function App() {
-	const [action, setAction] = React.useState(0)
-	
-	function onActionClick(id) {
-		setAction(id)
-	}
-
 	return (
         <main>
-			<Profile />
+			<Settings />
         </main>
     )
 }		
---
+++ rtime_craft/src/pages/Settings.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+import { Button } from '../components/Button';
+import { Switch } from '../components/Switch';
+
+export default function Settigns() {
+  return (
+    <div>
+      <Header />
+      <div className="relative flex items-center mt-10 min-h-[900px]">
+        <Navbar className=''/>
+        <div className='absolute top-3 left-24 font-monomaniac'>
+            <h2 className='font-monomaniac text-2xl ml-4 md:text-4xl'>Settings</h2>
+            <hr />
+            <div className='ml-5 mt-5'>
+                <h3 className='text-gray-600 text-xl font-semibold'>Theme</h3>
+                <div className="flex">
+                    <h4 className='ml-7 text-gray-600 text-lg'>Dark mode:</h4>
+                    <p className='ml-1 mt-1'>OFF</p>
+                </div>
+                    
+                    {/* <Switch id="dark-mode" /> */}
+                <h4 className='text-gray-600 text-xl font-semibold'>Profile</h4>
+                    <p className='ml-7 hover:underline text-gray-600 text-lg'>Change name</p>
+                    <p className='ml-7 hover:underline text-gray-600 text-lg'>Manage tasks</p>
+                    <p className='ml-7 hover:underline text-red-500 text-lg'>Delete account</p>
+                <h4 className='text-gray-600 text-xl font-semibold'>Timer</h4>
+                    <p className='ml-7 text-gray-600 text-lg'>Worked Timer type: <span className='hover:underline text-black'>Timer</span></p>
+                    <p className='ml-7 text-gray-600 text-lg'>Wasted Timer type: <span className='hover:underline text-black'>Stopwatch</span></p>
+            </div>
+            
+        </div>
+      </div>
+    </div>
+  )
+}
