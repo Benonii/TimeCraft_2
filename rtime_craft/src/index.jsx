98381538adb5a1cff4ee02b71a4e4a297462e50d
BENONI ESCKINDER <you@example.com>
2024-10-28 15:37:37 +0300

    Changed location of login + enabled login routing

--- rtime_craft/src/index.jsx
+++ rtime_craft/src/index.jsx
@@ -1,7 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from './queryClient.ts';
+
 import App from "./App"
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <StrictMode>
+        <QueryClientProvider client={new queryClient}>
+            <App />
+        </QueryClientProvider>
+    </StrictMode>,
+);
