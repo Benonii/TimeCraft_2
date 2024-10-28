03648d1b4122403e9d31a8ef5a9daf2c80f42330
BENONI ESCKINDER <besckinder@gmail.com>
2024-10-28 21:44:04 +0300

    Pushing backend changes

--- models/user.py
+++ models/user.py
@@ -8,7 +8,9 @@ from sqlalchemy import Column, String, Float, Integer
 class User(BaseModel, Base):
     ''' This class is the represantation for the User object '''
     __tablename__ = "users"
-    name = Column(String(128), nullable=False)
+    username = Column(String(128), nullable=False)
+    email = Column(String(128), nullable=True)
+    password = Column(String(255), nullable=True)
     weekly_work_hours_goal = Column(Float, nullable=False)
     number_of_work_days = Column(Integer, nullable=False)
     total_productive_time = Column(Float, nullable=False, default=0)
