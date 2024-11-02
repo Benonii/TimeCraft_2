6263cba6b70d97965bbe26129ad64c7d13b66f84
BENONI ESCKINDER <you@example.com>
2024-11-02 09:37:17 +0300

    Implementing delete user(completed)

--- rtime_craft/src/pages/Settings.tsx
+++ rtime_craft/src/pages/Settings.tsx
@@ -9,9 +9,11 @@ import DeleteUserAlert from '../components/custom/DeleteUserAlert';
 import ManageTasks from '../components/custom/modals/ManageTasks';
 import SuccessAlert from '../components/custom/SuccessAlert';
 import ErrorAlert from '../components/custom/ErrorAlert';
+import { useNavigate } from '@tanstack/react-router';
 
 export default function Settings() {
   const api = process.env.REACT_APP_API_URL;
+  const navigate = useNavigate();
   const { isDarkMode, toggleDarkMode } = useDarkMode();
   const [ success, setSuccess ] = useState<boolean>(false);
   const [ message, setMessage ] = useState<string>("");
@@ -29,11 +31,11 @@ export default function Settings() {
 
   const handleSuccess = () => {
     setSuccess(true);
-
-    setTimeout(() => {
-      setSuccess(false)
-    }, 3000);
- }
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    // navigate({ to: '/' }); // Redirect user before closing alert
+    // setTimeout(() => setSuccess(false), 3000); // Reset alert state
+}
 
   const handleError = () => {
       setError(true);
@@ -52,7 +54,7 @@ export default function Settings() {
     params.append('userId', user.id);
 
     const response = await fetch(`${api}/user/delete`, {
-      method: 'POST',
+      method: 'DELETE',
       headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
       },
@@ -83,6 +85,16 @@ export default function Settings() {
   return (
     <div>
       <Header />
+      {success && (
+        <div className='flex justify-center items-center w-[70%] max-w-[400px] ml-20'>
+          <SuccessAlert content={message} />
+        </div>
+      )}
+      {error && (
+        <div className='flex justify-center intems-center w-[70%] max-w-[400px] '>
+          <ErrorAlert content={message} />
+        </div>
+      )}
       <div className="relative flex items-center mt-10 min-h-[900px]">
         <Navbar className=''/>
         <div className='absolute top-3 left-24 font-monomaniac'>
@@ -102,7 +114,7 @@ export default function Settings() {
                     <h4 className='text-gray-600 text-xl font-semibold dark:text-gray-500'>Profile</h4>
                     <ChangeUsername />
                     <ManageTasks />
-                    <DeleteUserAlert />
+                    <DeleteUserAlert handleDelete={() => {mutation.mutate(user.id)}}/>
                   </>
                 )}
                 
