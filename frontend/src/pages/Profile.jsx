c9278d5071299c4750e7a3faddf2ca4366ec18f2
BENONI ESCKINDER <besckinder@gmail.com>
2024-11-05 21:35:22 +0300

    Roudning the numbers on the profile page a bit

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -20,9 +20,9 @@ function Profile() {
           <h2 className='font-monomaniac text-2xl ml-4 md:text-4xl'>User profile</h2>
           <div className='flex flex-col items-center justify-top ml-3 mt-5 bg-orange3 text-white rounded-lg h-48 md:h-64 w-[60vw] max-w-[500px]'>
             <p className='mt-2 font-monomaniac font-light text-2xl md:text-4xl'><span className='font-bold mr-1'>Username:</span> {user?.username}</p>
-            <p className='font-monomaniac font-light text-2xl md:text-4xl'><span className='font-bold mr-1'>TPT:</span> {user.tpt} hours</p>
-            <p className='font-monomaniac font-light text-2xl md:text-4xl'><span className='font-bold' mr-1>TWT:</span> {user.twt}</p>
-            <p className='font-monomaniac font-light text-2xl md:text-4xl'><span className='font-bold'>Efficiency:</span> {Number(user.tpt)/(Number(user.tpt) + Number(user.twt)) || 0}</p>
+            <p className='font-monomaniac font-light text-2xl md:text-4xl'><span className='font-bold mr-1'>TPT:</span> {user.tpt.toFixed(2)} hours</p>
+            <p className='font-monomaniac font-light text-2xl md:text-4xl'><span className='font-bold' mr-1>TWT:</span> {user.twt.toFixed(2)}</p>
+            <p className='font-monomaniac font-light text-2xl md:text-4xl'><span className='font-bold'>Efficiency:</span> {Number((user.tpt)/(Number(user.tpt) + Number(user.twt)) * 100).toFixed(2)|| 0}</p>
           </div>
           <div className='grid grid-cols-1 md:grid-cols-2 place-items-center absolute md:mt-0 w-[62vw] max-w-[500px] ml-1'>
             <Button variant='default' 
